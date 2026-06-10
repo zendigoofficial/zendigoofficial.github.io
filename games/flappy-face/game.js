2ae5ed8682c265582b5645b1b28a8a8748e1010c
zendigoofficial <zendigoofficial@gmail.com>
2026-06-10 07:55:33 -0400

    Update game.js

--- games/flappy-face/game.js
+++ games/flappy-face/game.js
@@ -1563,7 +1563,7 @@ function drawForegroundWall(){
     sourceCropTop -
     sourceCropBottom;
 
-  const scale = 0.25;
+  const scale = 0.15;
 
   const tileWidth =
     sourceWidth *
@@ -1573,7 +1573,7 @@ function drawForegroundWall(){
     sourceHeight *
     scale;
 
-  const wallLift = -25;
+  const wallLift = -40;
 
   const drawY =
     HEIGHT -
